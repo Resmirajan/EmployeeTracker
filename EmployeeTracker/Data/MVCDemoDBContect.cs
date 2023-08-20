66218f40c8e1e424c2adc5e90306421c3e5e5985
Resmi Rajan <sherlyrajan1992@gmail.com>
2023-08-21 00:37:49 +0100

    crud operation

---
+++ EmployeeTracker/Data/MVCDemoDBContect.cs
@@ -0,0 +1,14 @@
+﻿using EmployeeTracker.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeTracker.Data
+{
+    public class MVCDemoDBContect : DbContext
+    {
+        public MVCDemoDBContect(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
